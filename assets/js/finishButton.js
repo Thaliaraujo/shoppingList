df937cece7cabb1dc83eefe5f752dcf00a1d8a1f
Thaliaraujo <thalia.araujo321@gmail.com>
2023-08-09 15:51:53 -0300

    pull

--- assets/js/finishButton.js
+++ assets/js/finishButton.js
@@ -12,10 +12,21 @@ const finishButton = () => {
 
 const productCompleted = (evento) => {
     const finishButton = evento.target;
-
     const completed = finishButton.parentElement;
-
     completed.classList.toggle('done');
+
+    // Atualiza o status de conclusão no Local Storage
+    const itemText = completed.querySelector('.text').textContent;
+    const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+
+    const updatedList = shoppingList.map(item => {
+        if (item.value === itemText) {
+            item.done = !item.done; // Inverte o status de done
+        }
+        return item;
+    });
+
+    localStorage.setItem('shoppingList', JSON.stringify(updatedList));
 };
 
 export default finishButton;
